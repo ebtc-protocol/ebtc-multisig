8b32a4cec469a3d3a205d4b79921502605b508ff
sajanrajdev <sajanrajdev10@gmail.com>
2024-03-13 16:24:08 -0700

    feat: parameters parsing

--- helpers/utils.py
+++ helpers/utils.py
@@ -13,3 +13,11 @@ def approx(actual, expected, percentage_threshold):
     if diff == 0:
         return True
     return diff < (actual * percentage_threshold // 100)
+
+def str_to_bool(s):
+    if s.lower() == 'true':
+         return True
+    elif s.lower() == 'false':
+         return False
+    else:
+         raise ValueError("Cannot convert to boolean")
--- scripts/ebtc_governance.py
+++ scripts/ebtc_governance.py
@@ -1,6 +1,7 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 from helpers.constants import EmptyBytes32
+from helpers.utils import str_to_bool
 from rich.console import Console
 
 C = Console()
@@ -26,9 +27,10 @@ def cdpManager_set_staking_reward_split(value, salt=EmptyBytes32):
     """
     Set the staking reward split for the CDPManager contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setStakingRewardSplit\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setStakingRewardSplit({value})\n")
     safe.ebtc.cdpManager_set_staking_reward_split(value, salt)
 
     safe.post_safe_tx()
@@ -38,9 +40,10 @@ def cdpManager_set_redemption_fee_floor(value, salt=EmptyBytes32):
     """
     Set the redemption fee floor for the CDPManager contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionFeeFloor\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionFeeFloor({value})\n")
     safe.ebtc.cdpManager_set_redemption_fee_floor(value, salt)
 
     safe.post_safe_tx()
@@ -50,9 +53,10 @@ def cdpManager_set_minute_decay_factor(value, salt=EmptyBytes32):
     """
     Set the minute decay factor for the CDPManager contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setMinuteDecayFactor\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setMinuteDecayFactor({value})\n")
     safe.ebtc.cdpManager_set_minute_decay_factor(value, salt)
 
     safe.post_safe_tx()
@@ -62,9 +66,10 @@ def cdpManager_set_beta(value, salt=EmptyBytes32):
     """
     Set the beta for the CDPManager contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setBeta\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setBeta({value})\n")
     safe.ebtc.cdpManager_set_beta(value, salt)
 
     safe.post_safe_tx()
@@ -74,21 +79,23 @@ def cdpManager_set_redemptions_paused(pause, salt=EmptyBytes32):
     """
     Pause or unpause redemptions for the CDPManager contract.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused({pause})\n")
     safe.ebtc.cdpManager_set_redemptions_paused(pause, use_timelock=True, salt=salt)
 
     safe.post_safe_tx()
 
 
 def cdpManager_set_grace_period(value, salt=EmptyBytes32):
+    value = int(value)
     """
     Set the grace period for the CDPManager contract.
     """
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setGracePeriod\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setGracePeriod({value})\n")
     safe.ebtc.cdpManager_set_grace_period(value, salt)
 
     safe.post_safe_tx()
@@ -100,7 +107,7 @@ def priceFeed_set_fallback_caller(address, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for PriceFeed.setFallbackCaller\n")
+    C.print(f"\nUsing {safe.account} for PriceFeed.setFallbackCaller({address})\n")
     safe.ebtc.priceFeed_set_fallback_caller(address, salt)
 
     safe.post_safe_tx()
@@ -110,9 +117,10 @@ def priceFeed_set_collateral_feed_source(enable_dynamic_feed, salt=EmptyBytes32)
     """
     Set the collateral feed source for the PriceFeed contract.
     """
+    enable_dynamic_feed = str_to_bool(enable_dynamic_feed)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for PriceFeed.setCollateralFeedSource\n")
+    C.print(f"\nUsing {safe.account} for PriceFeed.setCollateralFeedSource({enable_dynamic_feed})\n")
     safe.ebtc.priceFeed_set_collateral_feed_source(enable_dynamic_feed, salt)
 
     safe.post_safe_tx()
@@ -124,10 +132,12 @@ def batch_collateral_feed_source_and_redemption_fee_floor(
     """
     Set the collateral feed source and redemption fee floor for the PriceFeed contract.
     """
+    enable_dynamic_feed = str_to_bool(enable_dynamic_feed)
+    new_fee_floor = int(new_fee_floor)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
     C.print(
-        f"\nUsing {safe.account} for PriceFeed.setCollateralFeedSource and CDPManager.setRedemptionFeeFloor\n"
+        f"\nUsing {safe.account} for PriceFeed.setCollateralFeedSource({enable_dynamic_feed}) and CDPManager.setRedemptionFeeFloor({new_fee_floor})\n"
     )
     safe.ebtc.batch_collateral_feed_source_and_redemption_fee_floor(
         enable_dynamic_feed, new_fee_floor, salt
@@ -140,9 +150,10 @@ def activePool_set_fee_bps(value, salt=EmptyBytes32):
     """
     Set the fee basis points for the ActivePool contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for ActivePool.setFeeBps\n")
+    C.print(f"\nUsing {safe.account} for ActivePool.setFeeBps({value})\n")
     safe.ebtc.activePool_set_fee_bps(value, salt)
 
     safe.post_safe_tx()
@@ -152,9 +163,10 @@ def borrowerOperations_set_fee_bps(value, salt=EmptyBytes32):
     """
     Set the fee basis points for the BorrowerOperations contract.
     """
+    value = int(value)
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFeeBps\n")
+    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFeeBps({value})\n")
     safe.ebtc.borrowerOperations_set_fee_bps(value, salt)
 
     safe.post_safe_tx()
@@ -166,7 +178,7 @@ def ebtcFeed_set_secondary_oracle(address, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for EBTCFeed.setSecondaryOracle\n")
+    C.print(f"\nUsing {safe.account} for EBTCFeed.setSecondaryOracle({address})\n")
     safe.ebtc.ebtcFeed_set_secondary_oracle(address, salt)
 
     safe.post_safe_tx()
@@ -185,7 +197,7 @@ def authority_set_role_name(role, name, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.setRoleName\n")
+    C.print(f"\nUsing {safe.account} for Authority.setRoleName({role}, {name})\n")
     safe.ebtc.authority_set_role_name(role, name, salt)
 
     safe.post_safe_tx()
@@ -195,9 +207,10 @@ def authority_set_user_role(user, role, enabled, salt=EmptyBytes32):
     """
     Set the user role for the Authority contract.
     """
+    enabled = str_to_bool(enabled)
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.setUserRole\n")
+    C.print(f"\nUsing {safe.account} for Authority.setUserRole({user}, {role}, {enabled})\n")
     safe.ebtc.authority_set_user_role(user, role, enabled, salt)
 
     safe.post_safe_tx()
@@ -209,9 +222,11 @@ def authority_set_role_capability(
     """
     Set the role capability for the Authority contract.
     """
+    role = int(role)
+    enabled = str_to_bool(enabled)
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.setRoleCapability\n")
+    C.print(f"\nUsing {safe.account} for Authority.setRoleCapability({role}, {target_address}, {functionSig}, {enabled})\n")
     safe.ebtc.authority_set_role_capability(
         role, target_address, functionSig, enabled, salt
     )
@@ -225,9 +240,10 @@ def authority_set_public_capability(
     """
     Set the public capability for the Authority contract.
     """
+    enabled = str_to_bool(enabled)
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.setPublicCapability\n")
+    C.print(f"\nUsing {safe.account} for Authority.setPublicCapability({target_address}, {functionSig}, {enabled})\n")
     safe.ebtc.authority_set_public_capability(
         target_address, functionSig, enabled, salt
     )
@@ -241,7 +257,7 @@ def authority_burn_capability(target_address, functionSig, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.burnCapability\n")
+    C.print(f"\nUsing {safe.account} for Authority.burnCapability({target_address}, {functionSig})\n")
     safe.ebtc.authority_burn_capability(target_address, functionSig, salt)
 
     safe.post_safe_tx()
@@ -253,7 +269,7 @@ def authority_set_authority(new_authority, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for Authority.setAuthority\n")
+    C.print(f"\nUsing {safe.account} for Authority.setAuthority({new_authority})\n")
     safe.ebtc.authority_set_authority(new_authority, salt)
 
     safe.post_safe_tx()
@@ -265,7 +281,7 @@ def ebtcFeed_set_primary_oracle(address, salt=EmptyBytes32):
     """
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for EBTCFeed.setPrimaryOracle\n")
+    C.print(f"\nUsing {safe.account} for EBTCFeed.setPrimaryOracle({address})\n")
     safe.ebtc.ebtcFeed_set_primary_oracle(address, salt)
 
     safe.post_safe_tx()
@@ -283,9 +299,10 @@ def cdpManager_set_redemptions_paused_techops(pause):
     """
     Pause or unpause redemptions for the CDPManager contract from the TechOps multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused({pause})\n")
     safe.ebtc.cdpManager_set_redemptions_paused(pause)
 
     safe.post_safe_tx()
@@ -295,9 +312,10 @@ def cdpManager_set_redemptions_paused_security(pause):
     """
     Pause or unpause redemptions for the CDPManager contract from the Security multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused\n")
+    C.print(f"\nUsing {safe.account} for CDPManager.setRedemptionsPaused({pause})\n")
     safe.ebtc.cdpManager_set_redemptions_paused(pause)
 
     safe.post_safe_tx()
@@ -307,9 +325,10 @@ def activePool_set_flash_loans_paused_techops(pause):
     """
     Pause or unpause flashloans for the ActivePool contract from the TechOps multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused\n")
+    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused({pause})\n")
     safe.ebtc.activePool_set_flash_loans_paused(pause)
 
     safe.post_safe_tx()
@@ -319,9 +338,10 @@ def activePool_set_flash_loans_paused_security(pause):
     """
     Pause or unpause flashloans for the ActivePool contract from the Security multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused\n")
+    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused({pause})\n")
     safe.ebtc.activePool_set_flash_loans_paused(pause)
 
     safe.post_safe_tx()
@@ -331,9 +351,10 @@ def borrowerOperations_set_flash_loans_paused_techops(pause):
     """
     Pause or unpause flashloans for the BorrowerOperations contract from the TechOps multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused\n")
+    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused({pause})\n")
     safe.ebtc.borrowerOperations_set_flash_loans_paused(pause)
 
     safe.post_safe_tx()
@@ -343,9 +364,10 @@ def borrowerOperations_set_flash_loans_paused_security(pause):
     """
     Pause or unpause flashloans for the BorrowerOperations contract from the Security multisig.
     """
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused\n")
+    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused({pause})\n")
     safe.ebtc.borrowerOperations_set_flash_loans_paused(pause)
 
     safe.post_safe_tx()
@@ -355,12 +377,12 @@ def pause_flashloans_techops(pause):
     """
     Pause or unpause flashloans for the ActivePool and BorrowerOperations contracts from the TechOps multisig.
     """
-
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.techops_multisig)
     safe.init_ebtc()
-    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused\n")
+    C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused({pause})\n")
     safe.ebtc.activePool_set_flash_loans_paused(pause)
-    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused\n")
+    C.print(f"\nUsing {safe.account} for BorrowerOperations.setFlashLoansPaused({pause})\n")
     safe.ebtc.borrowerOperations_set_flash_loans_paused(pause)
 
     safe.post_safe_tx()
@@ -370,7 +392,7 @@ def pause_flashloans_security(pause):
     """
     Pause or unpause flashloans for the ActivePool and BorrowerOperations contracts from the Security multisig.
     """
-
+    pause = str_to_bool(pause) 
     safe = GreatApeSafe(r.ebtc_wallets.security_multisig)
     safe.init_ebtc()
     C.print(f"\nUsing {safe.account} for ActivePool.setFlashloansPaused\n")
