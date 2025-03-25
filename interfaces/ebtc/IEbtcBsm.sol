bd9e2eaefc0c161af32222075d2b809e129c3a22
wtj2021 <wtj2022@proton.me>
2025-03-25 10:06:55 -0700

    adding more bsm roles

--- interfaces/ebtc/IEbtcBsm.sol
+++ interfaces/ebtc/IEbtcBsm.sol
@@ -6,4 +6,10 @@ interface IEbtcBsm {
     function setOraclePriceConstraint(address _newOraclePriceConstraint) external;
     function setRateLimitingConstraint(address _newRateLimitingConstraint) external;
     function setBuyAssetConstraint(address _newBuyAssetConstraint) external;
+    function setFeeToSell(uint256 _feeToSellBPS) external;
+    function setFeeToBuy(uint256 _feeToBuyBPS) external;
+    function pause() external;
+    function unpause() external;
+    function sellAssetNoFee(uint256 _assetAmountIn, address _recipient, uint256 _minOutAmount) external;
+    function buyAssetNoFee(uint256 _ebtcAmountIn, address _recipient, uint256 _minOutAmount) external;
 }
--- scripts/bsm_auth.py
+++ scripts/bsm_auth.py
@@ -39,25 +39,120 @@ def set_bsm_auth(safe):
             safe.ebtc.bsm.updateEscrow.signature,
             True,
         ),
-        # Role capabilities
         safe.ebtc.authority.setRoleCapability.encode_input(
             safe.ebtc.governance_roles.BSM_ADMIN.value,
             safe.ebtc.bsm,
             safe.ebtc.bsm.setOraclePriceConstraint.signature,
             True,
         ),
-        # Role capabilities
         safe.ebtc.authority.setRoleCapability.encode_input(
             safe.ebtc.governance_roles.BSM_ADMIN.value,
             safe.ebtc.bsm,
             safe.ebtc.bsm.setRateLimitingConstraint.signature,
             True,
         ),
-        # Role capabilities
         safe.ebtc.authority.setRoleCapability.encode_input(
             safe.ebtc.governance_roles.BSM_ADMIN.value,
             safe.ebtc.bsm,
             safe.ebtc.bsm.setBuyAssetConstraint.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_FEE_MANAGER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.setFeeToSell.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_FEE_MANAGER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.setFeeToBuy.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_PAUSER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.pause.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_PAUSER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.unpause.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_ESCROW_MANAGER.value,
+            safe.ebtc.bsm_escrow,
+            safe.ebtc.bsm_escrow.feeProfit.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_ESCROW_MANAGER.value,
+            safe.ebtc.bsm_escrow,
+            safe.ebtc.bsm_escrow.claimProfit.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_ESCROW_MANAGER.value,
+            safe.ebtc.bsm_escrow,
+            safe.ebtc.bsm_escrow.claimTokens.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_CONSTRAINT_MANAGER.value,
+            safe.ebtc.bsm_oracle_price_constraint,
+            safe.ebtc.bsm_oracle_price_constraint.setMinPrice.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_CONSTRAINT_MANAGER.value,
+            safe.ebtc.bsm_oracle_price_constraint,
+            safe.ebtc.bsm_oracle_price_constraint.setOracleFreshness.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_CONSTRAINT_MANAGER.value,
+            safe.ebtc.bsm_rate_limiting_constraint,
+            safe.ebtc.bsm_rate_limiting_constraint.setMintingConfig.signature,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_AUTHORIZED_USER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.unpause.sellAssetNoFee,
+            True,
+        ),
+        safe.ebtc.authority.setRoleCapability.encode_input(
+            safe.ebtc.governance_roles.BSM_AUTHORIZED_USER.value,
+            safe.ebtc.bsm,
+            safe.ebtc.bsm.unpause.buyAssetNoFee,
+            True,
+        ),
+        # Grant roles
+        safe.ebtc.authority.setUserRole.encode_input(
+            safe.ebtc.highsec_timelock,
+            safe.ebtc.governance_roles.BSM_ADMIN.value,
+            True,
+        ),
+        safe.ebtc.authority.setUserRole.encode_input(
+            safe.ebtc.lowsec_timelock,
+            safe.ebtc.governance_roles.BSM_FEE_MANAGER.value,
+            True,
+        ),
+        safe.ebtc.authority.setUserRole.encode_input(
+            safe.ebtc.security_multisig,
+            safe.ebtc.governance_roles.BSM_PAUSER.value,
+            True,
+        ),
+        safe.ebtc.authority.setUserRole.encode_input(
+            safe.ebtc.techops_multisig,
+            safe.ebtc.governance_roles.BSM_ESCROW_MANAGER.value,
+            True,
+        ),
+        safe.ebtc.authority.setUserRole.encode_input(
+            safe.ebtc.techops_multisig,
+            safe.ebtc.governance_roles.BSM_CONSTRAINT_MANAGER.value,
             True,
         ),
     ]
