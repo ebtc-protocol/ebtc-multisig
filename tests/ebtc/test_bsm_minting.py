e292e4c333a9823bea28f1784a3c24409fe6e079
wtj2021 <wtj2022@proton.me>
2025-04-26 18:52:26 -0700

    Giving extensible minting roles to BSM

---
+++ tests/ebtc/test_bsm_minting.py
@@ -0,0 +1,67 @@
+from brownie import chain
+import pytest
+from helpers.constants import EmptyBytes32
+from scripts.bsm_minting import set_bsm_minting
+
+
+def test_bsm_minting(security_multisig):
+    security_multisig.init_ebtc()
+
+    # before
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.mint.signature,
+        )
+        == False
+    )
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.burn.methods['address', 'uint'].signature,
+        )
+        == False
+    )
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.burn.methods['uint', ].signature,
+        )
+        == False
+    )
+
+    set_bsm_minting(security_multisig)
+
+    chain.sleep(security_multisig.ebtc.highsec_timelock.getMinDelay() + 1)
+    chain.mine()
+
+    set_bsm_minting(security_multisig)
+
+    # before
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.mint.signature,
+        )
+        == True
+    )
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.burn.methods['address', 'uint'].signature,
+        )
+        == True
+    )
+    assert (
+        security_multisig.ebtc.authority.canCall(
+            security_multisig.ebtc.bsm,
+            security_multisig.ebtc.ebtc_token,
+            security_multisig.ebtc.ebtc_token.burn.methods['uint', ].signature,
+        )
+        == True
+    )
